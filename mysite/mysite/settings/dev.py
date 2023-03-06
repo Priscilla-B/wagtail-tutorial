2733fb46aa46c69553dc9e7d681b98d4a784543e
Priscilla Baah <pyfbaah@gmail.com>
2023-03-06 23:45:19 +0000

    add project starter files

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zyu1m^nv+*-#jor=!c)f2=(pmmj+e78lxhh4z6y-ox(t)%2n@r"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
